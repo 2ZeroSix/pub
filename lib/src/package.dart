a976059f72ee772e6745b8152b2d6da4befef2ad
Bogdan Lukin <lukin.bogdan.a@gmail.com>
2022-06-05 18:57:57 +0700

    make assert methods static

--- lib/src/package.dart
+++ lib/src/package.dart
@@ -329,7 +329,7 @@ class Package {
     ).map(resolve).map(assertFileLinksResolvable).toList();
   }
 
-  void assertSymlinkLoop(
+  static void assertSymlinkLoop(
     String posixDir,
     String resolvedDir,
     Map<String, Set<String>> visitedSymlinks,
@@ -371,7 +371,7 @@ class Package {
     visitedSymlinks[posixDir] = currentSymlinks;
   }
 
-  String assertFileLinksResolvable(String path) {
+  static String assertFileLinksResolvable(String path) {
     if (!linkExists(path)) {
       return path;
     }
